a41cfa3d20090685ddaa6248a4783482e0cb72bf
Welton Leao Machado <wlmachado@bbmapfre.com.br>
2020-06-16 18:23:40 -0300

    Configuração de testes rest controller

---
+++ src/test/java/br/com/testes/estudo_tdd_bdd/EstudoTddBddApplicationTests.java
@@ -0,0 +1,15 @@
+package br.com.testes.estudo_tdd_bdd;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudoTddBddApplicationTests {
+
+	@Test
+	@DisplayName("Verifica se iniciou o servidor")
+	void contextLoads() {
+	}
+
+}
---
+++ src/test/java/br/com/testes/estudo_tdd_bdd/unit/BookControllerTest.java
@@ -0,0 +1,80 @@
+package br.com.testes.estudo_tdd_bdd.unit;
+
+
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import br.com.testes.estudo_tdd_bdd.dto.BookDTO;
+import br.com.testes.estudo_tdd_bdd.model.entity.Book;
+import br.com.testes.estudo_tdd_bdd.services.BookService;
+
+@ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
+@WebMvcTest
+@AutoConfigureMockMvc
+public class BookControllerTest {
+	
+	
+	static final String BOOK_API = "/books";
+	
+	@Autowired
+	MockMvc mockMvc;
+	
+	@MockBean
+	BookService bookService;
+	
+	
+	@Test
+	@DisplayName("Deve criar um livro com sucesso")
+	public void createBookTest() throws Exception{
+		BookDTO dto = BookDTO.builder()
+								.author("Arthur")
+								.title("As aventuras")
+								.isbn("001")
+								.build();
+		Book savedBook = Book.builder()
+								.id(10l)
+								.author("Arthur")
+								.title("As aventuras")
+								.isbn("001")
+								.build();
+		BDDMockito.given(bookService.save(Mockito.any(Book.class))).willReturn(savedBook);
+		String json = new ObjectMapper().writeValueAsString(dto);
+		MockHttpServletRequestBuilder request = MockMvcRequestBuilders
+						.post(BOOK_API)
+						.contentType(MediaType.APPLICATION_JSON)
+						.accept(MediaType.APPLICATION_JSON)
+						.content(json);
+		mockMvc.perform(request)
+			.andExpect(status().isCreated())
+			.andExpect(jsonPath("id").isNotEmpty())
+			.andExpect(jsonPath("title").value(dto.getTitle()))
+			.andExpect(jsonPath("author").value(dto.getAuthor()))
+			.andExpect(jsonPath("isbn").value(dto.getIsbn()));
+	}
+	
+	@Test
+	@DisplayName("Deve lançar um erro quando não passar na validação")
+	public void createInvalidBookTest() {
+		
+	}
+}
